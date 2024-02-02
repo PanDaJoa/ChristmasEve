f72e65767a2228d503e2f1044e32e1f743b8f440
KoSeungYeon <rhtmddus0121@naver.com>
2024-02-02 19:17:04 +0900

    [고승연] 엄청난 오류 수정 이유 알 수 없음

--- Assets/02.Scripts/Santa/Santa.cs
+++ Assets/02.Scripts/Santa/Santa.cs
@@ -6,6 +6,7 @@ using UnityEngine;
 
 public enum SantaType
 {
+    Resource,
     Bow,                
     Gun,                
     Sword,
@@ -30,12 +31,12 @@ public class Santa : MonoBehaviour
     }
     void Update()
     {
-            if (SantaHealth <= 0)           //»êÅ¸Á×À½
-            {
-                gameObject.SetActive(false);
-                Instantiate(SantaDeathPrefab, transform.position, transform.rotation);
-            }
-            if(SType == SantaType.Sword)
+        if (SantaHealth <= 0)           //»êÅ¸Á×À½
+        {
+            gameObject.SetActive(false);
+            Instantiate(SantaDeathPrefab, transform.position, transform.rotation);
+        }
+        if (SType == SantaType.Sword)
         {
             AttackDamage = 3;
         }
