ecfe80e7bebee44b759ca80f9050cd296c111da2
KoSeungYeon <rhtmddus0121@naver.com>
2024-02-04 15:29:15 +0900

    [고승연] 파일 위치 정리, GameReady,GameOver 텍스트 씬 뜨게 수정중

---
+++ Assets/02.Scripts/Ui/TextControl.cs
@@ -0,0 +1,26 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TextControl : MonoBehaviour
+{
+    public static TextControl instance;
+    public GameObject readyText;
+
+    private void Awake()
+    {
+        if(TextControl.instance == null)
+        {
+            
+        }
+    }
+    void Start()
+    {
+        
+    }
+
+    void Update()
+    {
+        
+    }
+}
