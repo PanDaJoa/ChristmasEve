2cb10c9b26446ca83033d8246d63246fb03d1342
이성민 <lsm7495@naver.com>
2024-02-05 10:16:28 +0900

    [이성민] Coin 클릭하면 파괴 구현

--- Assets/02.Scripts/Santa/Coin.cs
+++ Assets/02.Scripts/Santa/Coin.cs
@@ -17,19 +17,47 @@ public class Coin : MonoBehaviour
     }
         void Update()
     {
-     /*   CurrentTime += Time.deltaTime;
-        if (CurrentTime >= 5f)
+        // 마우스 왼쪽 버튼이 눌렸는지 확인
+        if (Input.GetMouseButtonDown(0))
         {
-            GameObject Coin = Instantiate(CoinPrefab);
-            Coin.SetActive(true);
-        }*/
+            // 마우스 포인터가 위치한 스크린 좌표를 Ray로 변환
+            Ray ray = Camera.main.ScreenPointToRay(Input.mousePosition);
+            RaycastHit2D hit = Physics2D.Raycast(ray.origin, ray.direction);
+
+            // 오브젝트가 클릭되었는지 확인
+            if (hit.collider != null)
+            {
+                // 클릭된 오브젝트가 Coin 태그를 가지고 있는지 확인
+                if (hit.collider.CompareTag("Coin"))
+                {
+                    // 클릭된 Coin 오브젝트의 동작 수행
+                    Coin coin = hit.collider.GetComponent<Coin>();
+                    if (coin != null)
+                    {
+                        coin.Collect(); // Coin 오브젝트에서 Collect() 메서드 호출
+                    }
+                }
+            }
+        }
+
+        /*   CurrentTime += Time.deltaTime;
+           if (CurrentTime >= 5f)
+           {
+               GameObject Coin = Instantiate(CoinPrefab);
+               Coin.SetActive(true);
+           }*/
     }
 
- /*   public void MakeItem()
-    {
-        GameObject Coin = Instantiate(CoinPrefab);
+    /*   public void MakeItem()
+       {
+           GameObject Coin = Instantiate(CoinPrefab);
 
-        Santa santa = GetComponent<Santa>();
-        Coin.transform.position = santa.transform.position;
-    }*/
+           Santa santa = GetComponent<Santa>();
+           Coin.transform.position = santa.transform.position;
+       }*/
+    public void Collect()
+    {
+        Destroy(gameObject);
+    }
+
 }
