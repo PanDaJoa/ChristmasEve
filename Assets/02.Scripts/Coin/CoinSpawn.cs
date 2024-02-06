1a0aa91664ce8caae61ac8d16dddf75d5324d887
KoSeungYeon <rhtmddus0121@naver.com>
2024-02-06 17:32:44 +0900

    [고승연] RunChildPrefab 추가

--- Assets/02.Scripts/Coin/CoinSpawn.cs
+++ Assets/02.Scripts/Coin/CoinSpawn.cs
@@ -28,7 +28,7 @@ public class CoinSpawn : MonoBehaviour
         void Update()
         {
             CurrentTime += Time.deltaTime;
-            if (CurrentTime >= 6f)
+            if (CurrentTime >= 4f)
             {
                 GameObject Coin = Instantiate(CoinPrefab);
                 Coin.SetActive(true);
